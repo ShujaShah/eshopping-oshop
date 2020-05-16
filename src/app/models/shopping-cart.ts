66fcbf3276a10bb630df8469385592b2e77c1ba8
ShujaShah <shuja.ashraf11@gmail.com>
2020-05-17 02:55:45 +0530

    AddedToShoppingPage

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,12 +1,26 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart { 
-  constructor(public items: ShoppingCartItem[]) {}
+  items: ShoppingCartItem[] = [];
+
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+    for (let productId in itemsMap) {
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for (let productId in this.items) 
+      sum += this.items[productId].totalPrice;
+    return sum;
+  }
   
   get totalItemsCount() {
     let count = 0;
-    for (let productId in this.items) 
-      count += this.items[productId].quantity;
+    for (let productId in this.itemsMap) 
+      count += this.itemsMap[productId].quantity;
     return count;
   }
-} 
+}
